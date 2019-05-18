8b8a8cd6b4c833f98160045c4b5b36f27437081f
iheb-brini <briniiheb153@gmail.com>
2019-05-18 16:28:44 +0100

    hello from dev team!

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World from dev team!"', () => {
+      expect(appController.getHello()).toBe('Hello World from dev team!');
     });
   });
 });
